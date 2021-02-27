e21d58074f9b018834efc786080e571c4332b41e
KRozumniak <kirkterra@gmail.com>
2021-02-27 00:40:09 -0500

    BookUpdateForm updated

---
+++ src/pages/book/BookModal.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form, Input } from 'antd';
+import BookUpdateForm from "@/pages/book/BookUpdateForm";
+
+export default function App (props: any) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="ghost" onClick={showModal}>
+        Update
+      </Button>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+
+        <BookUpdateForm
+          id={props.id}
+          showModal={setIsModalVisible}
+        />
+
+      </Modal>
+    </>
+  );
+};
---
+++ src/pages/book/BookUpdateForm.tsx
@@ -0,0 +1,69 @@
+import { Form, Input, Button } from 'antd';
+import { connect } from "umi";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+function bookUpdate (props: any) {
+  const [form] = Form.useForm();
+
+  const id = props.id;
+
+  const onFinish = (newName: any) => {
+    const obj = {
+      id,
+      newName
+    }
+    props.updateBook(obj)
+    console.log(id);
+    console.log(newName);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  return (
+    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item
+        name="name"
+        label="New name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit" onClick={() => props.showModal(false)}>
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+
+      </Form.Item>
+    </Form>
+  );
+};
+
+const mapDispatchToProps = (dispatch: any) => ({
+  updateBook: (obj: any) => dispatch({ type: "Book/updateBook", payload: obj }),
+});
+
+export default connect(null, mapDispatchToProps)(bookUpdate);
